8212ed246dc28a439af202440172dc029ebb0768
Diego Ordóñez Morales <diegordonez4@gmail.com>
2015-01-07 17:18:32 +0100

    [PREGUNTA] Dias de la semana
    Operaciones con switch, y uso de java.util.scanner.
    
    Comparaciones dentro de estructura “IF” requieren uso de “equals”.
    (Aviso Netbeans)

---
+++ src/DiasDeLaSeman.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author diegordonez
+ */
+import java.util.Scanner;
+
+public class DiasDeLaSeman {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        boolean salir = false;
+        
+        do {
+            System.out.print("Introduce dia de la semana: ");
+
+            int diaSemana = teclado.nextInt();
+
+            switch (diaSemana) {
+
+                case 1:
+                    System.out.println("El " + diaSemana + "er dia de la semana es Lunes!!");
+                    break;
+                case 2:
+                    System.out.println("El " + diaSemana + "do dia de la semana es Martes!!");
+                    break;
+                case 3:
+                    System.out.println("El " + diaSemana + "ro dia de la semana es Miercoles!!");
+                    break;
+                case 4:
+                    System.out.println("El " + diaSemana + "to dia de la semana es Jueves!!");
+                    break;
+                case 5:
+                    System.out.println("El " + diaSemana + "to dia de la semana es Viernes!!");
+                    break;
+                case 6:
+                    System.out.println("El " + diaSemana + "to dia de la semana es Sábado!!");
+                    break;
+                case 7:
+                    System.out.println("El " + diaSemana + "mo dia de la semana es Domingo!!");
+                    break;
+                default:
+                    System.out.println("Dia de la semana incorecto");
+                    break;
+            }
+            System.out.print("¿Continuar?[S/N]: ");
+            String opcion = teclado.next();
+            
+            if ("s".equals(opcion)) {
+                salir = true;
+            }else{
+                if ("n".equals(opcion)) {
+                    salir = false;
+                }
+            }
+        } while (salir == true);
+    }
+
+}
