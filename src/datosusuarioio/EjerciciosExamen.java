a353b379698abb3a01a1efdc55eec1c292a2b20d
Diego Ordóñez Morales <diegordonez4@gmail.com>
2015-01-29 19:59:28 +0100

    Producto de un número
    Calcular producto de dos números mediante la recursion

--- src/datosusuarioio/EjerciciosExamen.java
+++ src/datosusuarioio/EjerciciosExamen.java
@@ -29,6 +29,16 @@ public class EjerciciosExamen {
         int resSecfib = fibo(fibon);
         System.out.println("El valor de la posición: "+fibon+" es: "+resSecfib);
         
+        System.out.println("Introduce base del num que deseas calcular: ");
+        int bas = teclado.nextInt();
+        
+        System.out.println("Introduce exp del num que deseas calcular:");
+        int exp = teclado.nextInt();
+        
+        int rBasExp = producto( bas, exp);
+        
+        System.out.println("El reusltado es: "+rBasExp);
+        
     }
     public static int factorial(int num){
         
@@ -38,7 +48,7 @@ public class EjerciciosExamen {
         if (num == 0) { // Caso base
             res = 1;
         }else{
-            if(num > 0){
+            if(num > 0){// Evitar bucle infinito
                 res = num * factorial(num - 1);
             }
         }
@@ -48,12 +58,25 @@ public class EjerciciosExamen {
     public static int fibo(int pos){
         int numFib;
         
-        if (pos == 1 || pos == 2) {
+        if (pos == 1 || pos == 2) { // Caso base, primeras dos posiciones de la secuencia son uno
             numFib = 1;
         }else{
             numFib = fibo(pos - 2) + fibo(pos - 1);
         }
    
         return numFib;
+    }
+    
+    public static int producto(int bas, int exp){
+        int rslt = 0;
+        
+        if(exp == 0){
+            rslt = 0;
+        }else{
+            if(exp > 0){
+            rslt = bas + producto(bas, exp - 1);
+            }
+        }
+        return rslt;
     }
 }
